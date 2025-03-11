0a18f385b9a1456a47d9ef1afd29d1c3f28246d6
littlefishy521 <littlefishy521@yahoo.com>
2025-03-11 20:19:48 +0000

    factor

---
+++ 01_01b/factor.py
@@ -0,0 +1,10 @@
+def get_prime_factors(number):
+    factors = []
+    divisor = 2
+    while divisor <= number:
+        if number % divisor == 0:
+          factors.append(divisor)
+          number = number // divisor
+        else:
+            divisor += 1
+    return factors
